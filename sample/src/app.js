9507093c398387b04a8279ca9a38e132d174596e
marvelopers <marvelopers@gmail.com>
2021-08-18 23:03:38 +0900

    로더

--- sample/src/app.js
+++ sample/src/app.js
@@ -1,3 +1,10 @@
-import * as math from './math';
+import './app.css';
+import cute from './cute.jpeg';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.body.innerHTML = `
+    <img src="${cute}"/>
+  `
+})
 
 console.log(math.sum(1,2));
--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -8,5 +8,26 @@ module.exports = {
   output: {
     path : path.resolve('./dist'),
     filename: '[name].js'
+  },
+  module: {
+    rules:[
+      {
+        test: /\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+          // path.resolve('./my-webpack-loader.js')
+        ]
+      },
+      {
+        test: /\.(jpeg|png|gif|jpg|svg)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist',
+          name: '[name]/[ext]?[hash]',
+          limit: 20000,
+        }
+      }
+    ]  
   }
 }
