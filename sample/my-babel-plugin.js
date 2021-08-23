4ebf6a7e94c1e33e256a349885d59bb9bac135e9
marvelopers <marvelopers@gmail.com>
2021-08-23 22:43:57 +0900

    커스텀 바벨

---
+++ sample/my-babel-plugin.js
@@ -0,0 +1,12 @@
+module.exports = function myBabelPlugin() {
+  return {
+    VariableDeclaration(path) {
+      console.log('variableDeclaration() kind:', paht.node.kind);
+
+      if (path.node.kind === 'const') {
+        path.node.kind = 'var'
+      }
+    }
+  }
+
+}
